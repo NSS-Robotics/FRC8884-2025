5aa73463117efa21a2fb0b874229178b9ff10f59
Knight Owls <hello@knightowls.ca>
2025-01-27 11:23:42 -0500

    I am getting kicked off this computer

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+public class Elevator extends SubsystemBase {
+    
+}
