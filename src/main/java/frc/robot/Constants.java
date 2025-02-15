ec0e00013e9fb1b86ae45d4cd47f88559033ec0e
Knight Owls <hello@knightowls.ca>
2025-02-15 10:44:31 -0500

    e

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -223,7 +223,7 @@ public final class Constants {
   public static final class ClimberConstants {
     public static final int lMotorID = 20;
     public static final int rMotorID = 21;
-    public static final int lChannel = 9;
+    public static final int lChannel = 7;
     public static final int rChannel = 8;
     public static final double kP = 0;
     public static final double kI = 0;
