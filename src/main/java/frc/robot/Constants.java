f9ce80dddcb49383abdd12f273a0e4caa51bf342
Knight Owls <hello@knightowls.ca>
2025-03-03 17:10:24 -0500

    updated led length

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -364,7 +364,7 @@ public final class Constants {
 
     public static final class LEDConstants {
 
-        public static final int length = 151;
+        public static final int length = 154;
         public static final int channel = 9;
     }
 }
