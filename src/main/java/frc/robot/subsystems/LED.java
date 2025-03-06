de9d7329190e7f32b3106840fcf8aa26afcdbe15
Jacob <techsupport@kreindler.ca>
2025-03-05 21:27:28 -0500

    changed intake led colour

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -197,7 +197,7 @@ public class LED extends SubsystemBase {
     }
 
     public void intakeLeds() {
-        colour = Color.kBlue;
+        colour = Color.kHotPink;
 
         LEDPattern breathe = LEDPattern.solid(colour)
             .breathe(Seconds.of(.25))
